7b3de6a7e245062cd6e546d97056cb61aa4888f4
thenumbernine <christopher.e.moore@gmail.com>
2022-08-26 12:46:09 +0700

    turned it into a function

--- withorbit.lua
+++ withorbit.lua
@@ -1,4 +1,10 @@
+--[[
+I do this often enough that I'll put it in one place here
+I am returning it as a function so that it can be run multiple times without caching the result as unique .
+--]]
 local class = require 'ext.class'
 local OrbitBehavior = require 'glapp.orbit'
 local ImGuiApp = require 'imguiapp'
-return class(OrbitBehavior(ImGuiApp))
+return function()
+	return class(OrbitBehavior(ImGuiApp))
+end
