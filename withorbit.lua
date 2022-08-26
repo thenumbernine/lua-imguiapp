8cb1edc702005669d15ee9cf17e86b61d7e940e4
thenumbernine <christopher.e.moore@gmail.com>
2022-08-26 12:26:25 +0700

    oops

--- withorbit.lua
+++ withorbit.lua
@@ -1,4 +1,4 @@
-local class = require 'class'
+local class = require 'ext.class'
 local OrbitBehavior = require 'glapp.orbit'
 local ImGuiApp = require 'imguiapp'
 return class(OrbitBehavior(ImGuiApp))
